7a9dd512e94724cf1d614784831efb6905b9397e
Pauldao <daobatuoc93@gmail.com>
2020-07-18 11:28:04 +0700

    them ghi chu bai 11

--- exercise.py
+++ exercise.py
@@ -68,4 +68,11 @@ class Person:
 
 
 # câu 11:
-'''tuoc add them vai thu'''
+'''
+Viết một chương trình chấp nhận chuỗi từ do người dùng nhập vào, 
+phân tách nhau bởi dấu phẩy và in những từ đó thành chuỗi theo thứ tự bảng chữ cái,
+ phân tách nhau bằng dấu phẩy.
+
+Giả sử đầu vào được nhập là: without,hello,bag,world,
+thì đầu ra sẽ là: bag,hello,without,world.
+'''
